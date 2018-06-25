6be8b72f3b28d5b21af19ef474b0389561007eaf
anuosu <anuosunnuyi14@gmail.com>
2018-06-25 14:36:31 +0000

    automatically backed up by learn

--- example.rb
+++ example.rb
@@ -1,32 +1,13 @@
 class Person
 
-  def initialize(name,age,year)
-    @model = model
-    @make = make
-    @year = year
-    @carscount += 1
-    puts "I love my #{year} #{model}. it is a #{make}."
-  end
+  def initialize(name)
+    @name = name
+   
+  
+class Dog
+  
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
 
-  def stereo=(car_stereo)
-    @stereo = car_stereo
-  end
 
-  def stereo
-    @stereo
-  end
-
-  def initialize
-    @cam = backup_cam
-  end
-
-  def cam
-    @cam
-  end
-end
-
-toyota = Car.new("Camry", "Toyota", "1997")
-prius = Cars.new("Prius", "Toyota", "2011")
-prius.stereo = "Vimeo"
-
-puts "The car has a #{prius.stereo}"
